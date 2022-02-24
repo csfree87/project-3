8eace1145e1ea38b7ee5edbc80e2bfbc27247c4f
csfree87 <csfree@icloud.com>
2022-02-24 16:13:19 +0000

    Add board

--- run.py
+++ run.py
@@ -1,11 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 
+# opening statements and input
 def welcome():
     print('Welcome to the game')
+    player = input('Player Name: ')
 
-    player = input('Player Name: \n')
-   
+# setting up of boards
+
+def build_board(player_board):
+    return [['O' for count in range(player_board)] for count in range(player_board)]
+    build_board(10) 
+
+def print_board(board):
+    for b in board:
+        print(*b)
+
+board = build_board(10)
 
 welcome()
+print_board(board)
+
+
+
