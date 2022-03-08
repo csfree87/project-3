f6922b1987e594119c8d6c5289f2f959e47c8e7b
csfree87 <csfree@icloud.com>
2022-03-08 14:07:24 +0000

    Add function to check for a win during game

--- run.py
+++ run.py
@@ -21,4 +21,17 @@ def playing_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-playing_board()
+
+# Will check for winner during game
+def win_check(win):
+    if board[0] == board[1] == board[2] == win or \
+        board[0] == board[4] == board[8] == win or \
+        board[0] == board[3] == board[6] == win or \
+        board[1 == board[4] == board[7] == win or \
+        board[2] == board[4] == board[6] == win or \
+        board[2] == board[5] == board[8] == win or \
+        board[6] == board[7] == board[8] == win or \
+            board[3] == board[4] == board[5] == win:
+            return True
+    else:
+        return False
